6769bbfe0e2e3146e13bc12238869569f45e0128
Cat Gomez <catherinedgomez@gmail.com>
2020-03-12 23:12:18 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,7 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-   if age >= 0
+   if [age] >= 0
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
   end
